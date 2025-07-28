97a1d733f9b972f7e3ec917061b1e8b479ba3edb
Nos <zvezdazanatih@gmail.com>
2025-07-28 09:20:12 +0700

    29

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
   listenIp: '0.0.0.0',
   listenPort: 3000,
-  sslCrt: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-  sslKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
+  //sslCrt: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+  sslCrt: '/etc/letsencrypt/live/truruki.ru/fullchain.pem',
+  //sslKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
+  sslKey: '/etc/letsencrypt/live/truruki.ru/privkey.pem',
   mediasoup: {
     // Worker settings
     worker: {
